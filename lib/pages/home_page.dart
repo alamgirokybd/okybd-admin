f25481dd95f835128f275037fb2af8884da917d2
alamgirokybd <aiclassbdnsd@gmail.com>
2026-09-23 00:19:35 +0600

    Update timestamps in home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-[9/23/2026 12:09 AM] Okybd: import 'package:flutter/material.dart';
+[9/23/2026 12:18 AM] Okybd: import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
@@ -124,7 +124,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 ],
               ),
               const SizedBox(height: 12.0),
-[9/23/2026 12:09 AM] Okybd: ListView.builder(
+[9/23/2026 12:18 AM] Okybd: ListView.builder(
                 padding: EdgeInsets.zero,
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -224,7 +224,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(12.0),
-[9/23/2026 12:09 AM] Okybd: boxShadow: const [
+[9/23/2026 12:18 AM] Okybd: boxShadow: const [
           BoxShadow(
             color: Color(0x0F000000),
             blurRadius: 6.0,
