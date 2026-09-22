61d09a4a34e923cddbf69c3d1e117dd2ab7c1c13
alamgirokybd <aiclassbdnsd@gmail.com>
2026-09-22 20:15:54 +0600

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:go_router/go_router.dart';
 import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
