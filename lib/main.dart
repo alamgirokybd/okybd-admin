3a414332719a566dc09fa09a50458515836da429
alamgirokybd <aiclassbdnsd@gmail.com>
2026-09-24 01:25:43 +0600

    Make HomePageWidget const constructor

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
       ),
-      home: HomePageWidget(),
+      home: const HomePageWidget(),
     );
   }
 }
